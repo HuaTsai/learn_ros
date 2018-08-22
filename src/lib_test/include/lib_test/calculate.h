69eb30fff69b942c234505a49a163653b0376ed1
HuaTsai <a0910666734@gmail.com>
2018-08-23 02:43:29 +0800

    doxygen generate

--- src/lib_test/include/lib_test/calculate.h
+++ src/lib_test/include/lib_test/calculate.h
@@ -1,5 +1,10 @@
+/** \author HuaTsai */
+
 namespace lib_test {
+  /** \brief add function */
   int Add(int a, int b);
+  /** \brief minus function */
   int Minus(int a, int b);
+  /** \brief multiply function */
   int Multiply(int a, int b);
 }
--- src/lib_test/include/lib_test/rectangle.h
+++ src/lib_test/include/lib_test/rectangle.h
@@ -1,12 +1,27 @@
+/** \author HuaTsai */
+
 namespace lib_test {
+  //* A rectangle class
+  /**
+  * The basic rectangle class, it does:
+  * 1. calculate area
+  * 2. print above area
+  */
   class Rectangle {
    public:
+    /** \brief default constructor */    
     Rectangle();
+    /** \brief length and width constructor
+    * \param length   rectangle length
+    * \param length   rectangle width
+    */
     Rectangle(int length, int width);
+    /** \brief compute the area */
     int area();
+    /** \brief print the computed area */
     void print();
    private:
-    int length_;
-    int width_;
+    int length_;  /**< length */
+    int width_;  /**< width */
   };
 }
